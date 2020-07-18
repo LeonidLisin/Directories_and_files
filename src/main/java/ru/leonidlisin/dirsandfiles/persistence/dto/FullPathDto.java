3317716d8fc71ffb53b85e05a48331ac55de33bc
leonid <leo_lis@mail.ru>
2020-07-18 19:33:47 +0300

    Добавил DTO для пути

---
+++ src/main/java/ru/leonidlisin/dirsandfiles/persistence/dto/FullPathDto.java
@@ -0,0 +1,10 @@
+package ru.leonidlisin.dirsandfiles.persistence.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FullPathDto {
+    private String fullPath;
+}
