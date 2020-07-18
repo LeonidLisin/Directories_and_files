3332b272776478460a82c31d23b9339ddc852964
leonid <leo_lis@mail.ru>
2020-07-18 20:43:04 +0300

    добавил сохранение в БД списка директорий и файлов

--- src/main/java/ru/leonidlisin/dirsandfiles/services/FileService.java
+++ src/main/java/ru/leonidlisin/dirsandfiles/services/FileService.java
@@ -2,12 +2,36 @@ package ru.leonidlisin.dirsandfiles.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.leonidlisin.dirsandfiles.persistence.entities.File;
+import ru.leonidlisin.dirsandfiles.persistence.entities.FullPath;
 import ru.leonidlisin.dirsandfiles.persistence.repositories.FileRepository;
+
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
     private final FileRepository fileRepository;
+
+    public void save(List<Path> pathes, FullPath fullPath) throws IOException {
+        for(Path p: pathes){
+            boolean isDir = Files.isDirectory(p);
+            long size = -1;
+            if (!isDir) size = FileChannel.open(p).size();
+            fileRepository.save(
+                    File.builder()
+                    .name(p.getFileName().toString())
+                    .size(size)
+                    .fullPath(fullPath)
+                    .isDir(isDir)
+                    .build()
+            );
+        }
+    }
 
 }
--- src/main/java/ru/leonidlisin/dirsandfiles/services/FullPathService.java
+++ src/main/java/ru/leonidlisin/dirsandfiles/services/FullPathService.java
@@ -7,27 +7,51 @@ import ru.leonidlisin.dirsandfiles.persistence.entities.FullPath;
 import ru.leonidlisin.dirsandfiles.persistence.repositories.FullPathRepository;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FullPathService {
 
     private final FullPathRepository fullPathRepository;
+    private final FileService fileService;
 
-    public List<FullPath> getFileList(){
+    public List<FullPath> getFileListFromDb(){
         return fullPathRepository.findAll();
     }
 
     @Transactional
-    public void save(FullPathDto fullPathDto){
+    public void save(FullPathDto fullPathDto) throws IOException {
         FullPath fullPath = FullPath.builder()
                 .fullPath(fullPathDto.getFullPath())
                 .date(new Date())
                 .build();
 
         fullPathRepository.save(fullPath);
+        fileService.save(getDirContent(fullPathDto.getFullPath()), fullPath);
+    }
+
+    private List<Path> getDirContent(String fullPath) throws IOException {
+        List<Path> folders =  Files.list(Paths.get(fullPath))
+                .filter(p -> Files.isDirectory(p))
+                .sorted((Comparator.naturalOrder()))
+                .collect(Collectors.toList());
+
+        List<Path> files =  Files.list(Paths.get(fullPath))
+                .filter(p -> !Files.isDirectory(p))
+                .sorted((Comparator.naturalOrder()))
+                .collect(Collectors.toList());
+        List<Path> all = new ArrayList<>(folders);
+        all.addAll(files);
+        return all;
     }
 
 }
