44258f379bc8014ed545e6aeb0fcd9c919a5d524
leonid <leo_lis@mail.ru>
2020-07-18 12:47:06 +0300

    Создан базовый класс

---
+++ src/main/java/ru/leonidlisin/dirsandfiles/DirsandfilesApplication.java
@@ -0,0 +1,13 @@
+package ru.leonidlisin.dirsandfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirsandfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirsandfilesApplication.class, args);
+	}
+
+}
