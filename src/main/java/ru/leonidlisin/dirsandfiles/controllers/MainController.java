b1f26d5d171195a67b5b66a9af7cb67cc66ae4ff
leonid <leo_lis@mail.ru>
2020-07-19 11:28:31 +0300

    добавил вывод на фронт списка файлов для выбранной директории

--- src/main/java/ru/leonidlisin/dirsandfiles/controllers/MainController.java
+++ src/main/java/ru/leonidlisin/dirsandfiles/controllers/MainController.java
@@ -2,14 +2,18 @@ package ru.leonidlisin.dirsandfiles.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.leonidlisin.dirsandfiles.persistence.dto.FullPathDto;
+import ru.leonidlisin.dirsandfiles.persistence.entities.File;
 import ru.leonidlisin.dirsandfiles.services.FullPathService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +22,8 @@ public class MainController {
     private final FullPathService fullPathService;
 
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("fullPathList", fullPathService.obtainFullPathList());
         return "index";
     }
 
@@ -26,6 +31,13 @@ public class MainController {
     public void addDirectoryPath(FullPathDto fullPathDto, HttpServletRequest request, HttpServletResponse response) throws IOException {
         fullPathService.save(fullPathDto);
         response.sendRedirect(request.getHeader("referer"));
+    }
+
+    @PostMapping("/files/{id}")
+    public String getFiles(@PathVariable UUID id, Model model) {
+        model.addAttribute("title", fullPathService.getFullPathById(id).getFullPath());
+        model.addAttribute("fileList", fullPathService.getFullPathById(id).getFiles());
+        return "files";
     }
 
 }
